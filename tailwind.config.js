313fef69b244d048b70157e9d4c1c1ef0d915469
Felipe Gomez <luisfelipe_1002@hotmail.com>
2025-01-07 18:42:11 -0500

    Tailwind como API para la creación de un Design System

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 module.exports = {
   content: ['./public/index.html', './src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bali': "url('../img/bali.jpg')",
+        'chicago': "url('../img/chicago.jpg')",
+        'europe': "url('../img/europe.jpg')",
+        'iceland': "url('../img/iceland.jpg')",
+        'LA': "url('../img/LA.jpg')",
+        'miami': "url('../img/miami.jpg')",
+        'new_york': "url('../img/new_york.jpg')",
+        'norway': "url('../img/norway.jpg')",
+        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+        'seattle': "url('../img/seattle.jpg')",
+        'switzerland': "url('../img/switzerland.jpg')",
+        'sydney': "url('../img/sydney.jpg')",
+        'tosemite': "url('../img/yosemite.jpg')",
+      }, 
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9'
+      }),
+      textColor: {
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9'
+      },
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif' ]
+      }
+    },
   },
   plugins: [],
 }
